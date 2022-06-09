5f1426f5141646c5569386933ca330435cb91604
+ <+>
2022-06-09 16:04:49 +0300

    commit tree 9364c7d9d132e93a60c9cd7543451fc0235869c3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin() {
+  const record = {
+    year: "2015",
+    result: "W",
+
+  };
+}
+superbowlWin =(record)=>{
+    const result=record.find(record=>record.result==="W");
+    return !!result? result.year:undefined;
+   
+}
